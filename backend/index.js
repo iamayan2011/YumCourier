968e81b36dc48afbc70824fe9e99bf2d347a4fbc
Ayan <ayan.raza.2011@gmail.com>
2024-03-27 21:46:51 +0530

    added frontend link

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
 
 const express = require('express')
 const app = express()
+const cors = require("cors");
 
 const mongoDB = require("./db");
 require('dotenv').config()
 
 mongoDB();
 
+const corsOptions = {
+  origin: "https://yumcourier.onrender.com/", // frontend URI (ReactJS)
+}
+
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://yumcourier.onrender.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 })
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
